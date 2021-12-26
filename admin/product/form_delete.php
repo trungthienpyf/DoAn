8ab06bfd2a1aa2d96680eaaa44cb2c4a76c4c367
trungthienpyf <269.tdd@gmail.com>
2021-12-26 16:49:05 +0700

    update

---
+++ admin/product/form_delete.php
@@ -0,0 +1,13 @@
+<?php require'../meu_top.php'?>
+
+<?php require '../connect.php';
+
+
+$id=$_GET['id'];
+$sql="select * from product where id='$id'";
+$result=mysqli_query($connect,$sql);
+$each=mysqli_fetch_array($result);
+?>
+
+
+<?php require'../menu_bottom.php'?>
--- admin/product/process_delete.php
+++ admin/product/process_delete.php
@@ -4,8 +4,19 @@ require '../connect.php';
 $id=$_GET['id'];
 $sql="delete  from product 
 where id='$id'";
+
+
+
+$sql_delete="select * from product where id='$id'";
+$result_delete=mysqli_query($connect,$sql_delete);
+$each=mysqli_fetch_array($result_delete);
+
+
+$folder = "photos/";
+$path_file = $folder . $each['img'];
+
+unlink($path_file);
 mysqli_query($connect,$sql);
-
+    
 mysqli_close($connect);
-
 header('location:index.php?success=Xóa thành công');

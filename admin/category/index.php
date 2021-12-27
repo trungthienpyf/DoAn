5d2b41e0007a01f070416d96051b53148306df1d
trungthienpyf <269.tdd@gmail.com>
2021-12-27 20:08:54 +0700

    update

--- admin/category/index.php
+++ admin/category/index.php
@@ -9,17 +9,17 @@ $result=mysqli_query($connect,$sql);
 		<a style="float:right;" href="form_insert.php">Thêm thể loại</a>
 		<table border="1" width="100%">
 			<tr>
-				<th width="40%">id</th>
-				<th width="40%">Tên</th>
-				<th width="10%">Sửa</th>
-				<th width="10%">Xóa</th>
+				<th >id</th>
+				<th >Tên</th>
+				<!-- <th width="10%">Sửa</th>
+				<th width="10%">Xóa</th> -->
 			</tr>
 			<?php foreach ($result as $key => $each) { ?>
 				<tr>
 					<td><?php echo $key+1 ?></td>
 					<td><?php echo $each['name']?></td>	
-					<td><a href="form_update.php?id=<?php echo $each['id']?>">Sửa</a></td>	
-					<td><a href="process_delete.php?id=<?php echo $each['id']?>">Xóa</a></td>	
+					<!-- <td><a href="form_update.php?id=<?php echo $each['id']?>">Sửa</a></td>	
+					<td><a href="process_delete.php?id=<?php echo $each['id']?>">Xóa</a></td>	 -->
 				</tr>
 			<?php } ?>
 		</table>
--- admin/manufactures/index.php
+++ admin/manufactures/index.php
@@ -14,21 +14,21 @@ $result=mysqli_query($connect,$sql);
 ?>
 	<table border="1" width="100%">
 		<tr>
-			<th width="40%">id</th>
-			<th width="40%">tên</th>
-			<th width="10%">Sửa</th>
-			<th width="10%">Xóa</th>
+			<th>id</th>
+			<th>tên</th>
+			<!-- <th>Sửa</th>
+			<th>Xóa</th> -->
 		</tr>
 		<?php foreach ($result as $key => $each) {?>
 		<tr>
 			<td><?php echo  $key+1  ?></td>
 			<td><?php echo $each['name'] ?></td>
-			<td>
+			<!-- <td>
 				<a href="form_update.php?id=<?php echo $each['id']?>">Sửa</a>
 			</td>
 			<td>
 				<a href="process_delete.php?id=<?php echo $each['id']?>">Xóa</a>
-			</td>
+			</td> -->
 		</tr>
 		<?php } ?>
 	
